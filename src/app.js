aa4b67a607757d6841eb615a3eb059b1d046f54a
shalehin <developer.shalehin@gmail.com>
2025-11-20 15:59:27 +0600

    hero section

---
+++ src/app.js
@@ -0,0 +1,16 @@
+let navbar = document.querySelector('.navBar');
+let navbtn = document.querySelector('.navitagation-btn i')
+
+navbtn.addEventListener('click', () => {
+    if(navbar.classList.contains('-right-full') && navbtn.classList.contains('fa-bars')){
+        navbar.classList.remove('-right-full')
+        navbar.classList.add('right-0')
+        navbtn.classList.remove('fa-bars');
+        navbtn.classList.add('fa-xmark')
+    }else{
+        navbar.classList.add('-right-full')
+        navbar.classList.remove('right-0')
+        navbtn.classList.add('fa-bars');
+        navbtn.classList.remove('fa-xmark')
+    }
+})
